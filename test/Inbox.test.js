85e82a1815dd7281c30e16d4fb2aaaa8e861fc67
stanleychen86 <47363544+stanleychen86@users.noreply.github.com>
2020-12-22 21:49:49 -0600

    deployment with web3
    use one of the accounts to deploy the contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
 
 // class Car{
 //     park() {
@@ -33,18 +35,23 @@ const web3 = new Web3(ganache.provider());
 // });
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     // get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
     //use one of the accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!'] })
+    .send({ from : accounts[0], gas: '1000000'});
+
 
 });
 
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 });
