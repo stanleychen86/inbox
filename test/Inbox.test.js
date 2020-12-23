b455abec607ee2d0de8a980ae9586fc0938bbb2b
stanleychen86 <47363544+stanleychen86@users.noreply.github.com>
2020-12-22 21:36:23 -0600

    tried ganache to test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,50 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+// class Car{
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive(){
+//         return 'vroom';
+//     }
+// }
+
+
+// let car;
+
+// beforeEach(() => {
+//     car = new Car();
+// })
+
+// describe('Car', () => {
+//     it('can park', () => {
+//         const car = new Car();
+//         assert.equal(car.park(),  'stopped');
+//     });
+
+//     it('can drive', () => {
+//         const car = new Car();
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// });
+
+let accounts;
+
+beforeEach(async () => {
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    //use one of the accounts to deploy the contract
+
+});
+
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
+});
